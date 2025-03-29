028d3e292bd9ebad4517793feb1676da510c095a
shinichi15 <duynv.coder@gmail.com>
2025-03-29 16:27:53 +0700

    Add Header

--- assets/js/dungchung.js
+++ assets/js/dungchung.js
@@ -10,3 +10,24 @@ function date_content() {
         console.error("Không tìm thấy phần tử #date__container");
     }
 }
+
+// Thêm từ khóa tìm kiếm
+function addTags(nameTag, link) {
+    var new_tag = `<a href=` + link + `>` + nameTag + `</a>`;
+
+    // Thêm <a> vừa tạo vào khung tìm kiếm
+    var khung_tags = document.getElementsByClassName('tags')[0];
+    khung_tags.innerHTML += new_tag;
+}
+
+function searchData() {
+    const search_btn = document.getElementById("search_btn");
+
+    search_btn.addEventListener("click", function () {
+        const search_data = document.getElementById("search__box").value.trim();
+        if (search_data) {
+            window.location.href = `search.html?q=${encodeURIComponent(search_data)}`;
+        }
+});
+
+}
--- assets/js/trangchu.js
+++ assets/js/trangchu.js
@@ -1,3 +1,7 @@
 window.addEventListener("load", function () {
     date_content();
+    searchData();
+
+    var tags = ["Mobile", "Laptop", "Camera", "Tablet", "Computer", "Tivi", "Printer", "PC"];
+    for(var t of tags) addTags(t, "index.html?search=" + t)
 });
