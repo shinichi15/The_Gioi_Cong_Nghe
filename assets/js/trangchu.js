bc48a5132a7b41ed1f1e8438962dd6dde5ed1b81
shinichi15 <duynv.coder@gmail.com>
2025-04-04 16:46:56 +0700

    Fix Ui/UX

--- assets/js/trangchu.js
+++ assets/js/trangchu.js
@@ -1,7 +1,4 @@
 window.addEventListener("load", function () {
     date_content();
     searchData();
-
-    var tags = ["Mobile", "Laptop", "Camera", "Tablet", "Computer", "Tivi", "Printer", "PC"];
-    for(var t of tags) addTags(t, "index.html?search=" + t)
 });
